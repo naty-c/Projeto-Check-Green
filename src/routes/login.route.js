d140d3664476e523344b39b0c931af62de813254
Natalia Cagnani <nataliacagnani@Natalias-MacBook-Air.local>
2024-05-06 16:47:49 -0300

    Adicionando controllers

--- src/routes/login.route.js
+++ src/routes/login.route.js
@@ -1,66 +1,8 @@
-const { Router } = require('express')
-const User = require('../models/User');
-const bcrypt = require('bcryptjs');
-const { sign } = require('jsonwebtoken');
+const { Router } = require('express');
+const LoginController = require('../controllers/LoginController');
 
 const loginRoutes = new Router();
 
-loginRoutes.post('/', async (req, res) => {
-
-    /*
-   #swagger.tags = ['Login'],
-   #swagger.summary = 'Create new login'
-   #swagger.parameters['body'] = {
-       in: 'body',
-       description: 'This endpoint will create login for a new user',
-       schema: {
-           $email: 'yuki@email.com',
-           $password: 'yukipassword'
-       }
-   #swagger.responses: {
-       '201': {
-           description: Ok
-       }
-       '401': {
-           description: Invalid password
-       }
-       '404': {
-           description: User not found
-       }
-       '500': {
-           description: Failed to login
-       }
-   }
-*/
-
-    try {
-        const { email, password } = req.body;
-        console.log(email, password);
-        // Look for the user through the email
-        const user = await User.findOne({ 
-            where: { email:email } });
-
-        // Verify if the user was found
-        if (!user) {
-            return res.status(404).json({ message: 'User not found' });
-        }
-
-        // Compare the password and the hashed password
-        const match = await bcrypt.compare(password, user.password);
-
-        if (!match) {
-            return res.status(401).json({ message: 'Invalid password' });
-        }
-
-        // Generate a JWT token
-        const token = sign({ email: user.email, id: user.id }, 
-            process.env.SECRET_JWT, { expiresIn: "1d" });
-        return res.status(200).send({ token });
-
-    } catch (error) {
-        console.error('Login error:', error.message);
-        return res.status(500).json({ message: "Failed to login" });
-    }
-});
+loginRoutes.post('/', LoginController.login);
 
 module.exports = loginRoutes;
